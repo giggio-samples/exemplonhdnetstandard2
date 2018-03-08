cee6f3d6ce999206c5dd5b10770b576eb4a7c7c2
Giovanni Bassi <giggio@giggio.net>
2018-03-08 10:29:36 -0800

    Add project

---
+++ nh/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nh
+{
+public class City
+    {
+        public virtual int Id { get;  set; }
+        public virtual long Population { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
--- nh/Program.cs
+++ nh/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NHibernate.Cfg;
+using System;
+using System.Linq;
 
 namespace nh
 {
@@ -6,7 +8,33 @@ namespace nh
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new Configuration();
+            configuration.Configure();
+            var SessionFactory = configuration.BuildSessionFactory();
+            var session = SessionFactory.OpenSession();
+
+            Console.Write("Digite o nome da Cidade =");
+            var name = Console.ReadLine();
+
+            Console.Write("Digite a populaçao=");
+            var population = Console.ReadLine();
+
+
+            var city = new City { Name = name, Population = Convert.ToInt16(population) };
+            session.SaveOrUpdate(city);
+            session.Flush();
+
+            var cities = session.Query<City>()
+                .Where(p => p.Population > 1000)
+                .ToList();
+
+            Console.WriteLine($"Cidades com mais de 1000 pessoas:");
+            cities.ForEach(x =>
+            {
+                Console.WriteLine($"{x.Name} -> {x.Population}");
+            });
+            Console.ReadLine();
+            session.Close();
         }
     }
 }
