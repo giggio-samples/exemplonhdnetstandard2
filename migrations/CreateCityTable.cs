1766a72d00024ad5a52d9144c3882d4ca86fd2f6
Giovanni Bassi <giggio@giggio.net>
2018-04-13 11:38:38 -0300

    Add Fluent Migrator (beta)

---
+++ migrations/CreateCityTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace migrations
+{
+    [Migration(0)]
+    public class CreateCityTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("City")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().Nullable()
+                .WithColumn("Population").AsInt64().Nullable();
+        }
+        public override void Down()
+        {
+            Delete.Table("City");
+        }
+
+    }
+}
